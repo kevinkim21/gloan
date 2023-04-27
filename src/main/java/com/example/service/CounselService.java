67796daad5cfc9f54b9c8e7e0bf4a283f3b85033
Gavin kim <dreamworld612@gmail.com>
2023-04-27 16:09:55 +0900

    add update counsel

--- src/main/java/com/example/service/CounselService.java
+++ src/main/java/com/example/service/CounselService.java
@@ -6,4 +6,5 @@ import com.example.dto.CounselDTO.Response;
 public interface CounselService {
   Response create(Request request);
   Response get(Long counselId);
+  Response update(Long counselId, Request request);
 }
